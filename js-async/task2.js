6b06f4b5e7b187d7b813963981d3c79e4f5d3d97
Alisa-Shevchenko <alisa.shevchenko747@gmail.com>
2023-10-19 10:29:40 +0300

    Add HW-8. JS async & json

---
+++ js-async/task2.js
@@ -0,0 +1,23 @@
+// Tack2
+// Створіть асинхронну функцію delayAndLog(message, milliseconds), яка приймає повідомлення message 
+// і час у мілісекундах milliseconds.
+// Функція повинна затримати виконання на заданий час milliseconds, використовуючи setTimeout.
+// Після затримки, функція повинна вивести message на консоль.
+// Використайте async/await для керування асинхронним виконанням функції.
+// Викличте функцію delayAndLog з різними повідомленнями та затримками.
+
+async function delayAndLog(message, milliseconds) {
+
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+    await delay(milliseconds);
+
+    console.log(message);
+}
+async function main() {
+    await delayAndLog("delay 2 seconds", 2000);
+    await delayAndLog("delay 3 seconds", 3000);
+    await delayAndLog("delay 1 second", 1000);
+}
+
+main();
