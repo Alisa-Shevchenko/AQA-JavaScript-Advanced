1f8faee22e47f74d3c65faef3ec5c773fe36db94
Alisa-Shevchenko <alisa.shevchenko747@gmail.com>
2023-10-25 15:56:26 +0300

    Add HW-10. Selectors

---
+++ selectors/selectors.js
@@ -0,0 +1,16 @@
+
+// Перетягніть цей файл у браузер 
+// Напишіть 10 будь яких селекторів на сторінці
+// Завдання можете прикріпити як текстовий файл до домашки, або додати на гіт собі
+
+// .header
+// h1[data-cy="header"]
+// #button1
+// #about
+// .description
+// .team-list li:nth-child(1)
+// .contact-info a
+// div:nth-child(2) a
+// .contact-info p:nth-child(2)
+// input[type="email"]
+// #message
